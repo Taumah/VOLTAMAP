3bf7fff07e6ed76aa477478c033633d509c2b8c4
Thomas Tresgots <ttresgots@gmail.com>
2022-05-15 21:53:19 +0200

    add staging zone tables

--- data/base.sql
+++ data/base.sql
@@ -5,3 +5,20 @@ create table stationID(
     googleID varchar(50),
     tomtomID varchar(50)
 );
+
+create table stz_googleAPI(
+    id int auto_increment primary key,
+    api_id varchar(80) not null,
+    latitude double not null ,
+    longitude double not null,
+    insert_time timestamp not null default current_timestamp
+);
+
+create table stz_tomtomAPI(
+    id int auto_increment primary key,
+    api_id varchar(80) not null,
+    latitude double not null ,
+    longitude double not null,
+    insert_time timestamp default current_timestamp
+);
+
