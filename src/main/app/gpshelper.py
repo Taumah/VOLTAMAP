19e4280ef294971aae990d440311a0dcf907a31d
quentin-pierson <quentinprs62@gmail.com>
2022-05-23 22:21:59 +0200

    reformat file

--- src/main/app/gpshelper.py
+++ src/main/app/gpshelper.py
@@ -1,21 +1,22 @@
 from kivy.app import App
-from kivymd.uix.dialog import MDDialog
 from kivy.utils import platform
+from kivymd.uix.dialog import MDDialog
 
 
 class GpsHelper():
     has_centered_map = False
 
     def run(self):
+        print("Je suis sur la plateform", platform)
+
         # Get a reference to GpsBlinker, then call blink()
-        gps_blinker = App.get_running_app().root.ids.home_screen.ids.mapview.ids.blinker
+        gps_blinker =  App.get_running_app().root.ids.home_screen.ids.mapview.ids.blinker
         # Start blinking the GpsBlinker
         gps_blinker.blink()
 
         # Request permissions on Android
         if platform == 'android':
-            from android import request_permissions, Permission
-
+            from android.permissions import Permission, request_permissions
             def callback(permission, results):
                 if all([res for res in results]):
                     print("Got all permissions")
@@ -35,6 +36,9 @@ class GpsHelper():
             gps.configure(on_location=self.update_blinker_position,
                           on_status=self.on_auth_status)
             gps.start(minTime=1000, minDistance=0)
+        if platform == 'macosx':
+            print("je suis dans mac os")
+
 
     def update_blinker_position(self, *args, **kwargs):
         my_lat = kwargs['lat']
@@ -51,8 +55,6 @@ class GpsHelper():
             map.center_on(my_lat, my_lon)
             self.has_centered_map = True
 
-        App.get_running_app().current_lat = my_lat
-        App.get_running_app().current_lon = my_lon
 
     def on_auth_status(self, general_status, status_message):
         if general_status == 'provider-enabled':
@@ -65,3 +67,5 @@ class GpsHelper():
         dialog.size_hint = [.8, .8]
         dialog.pos_hint = {'center_x': .5, 'center_y': .5}
         dialog.open()
+
+
--- src/main/app/main.py
+++ src/main/app/main.py
@@ -1,19 +1,24 @@
-import pymysql
+# Kivy
+from kivy.uix.screenmanager import Screen
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivymd.uix.behaviors import FakeRectangularElevationBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
-from kivy.uix.screenmanager import Screen, NoTransition, CardTransition
 
+# Class
 from homemapview import HomeMapView
 from gpshelper import GpsHelper
 from src.main.data.connectors import RDSconnector
+
+#  SQl
+import pymysql
 
 Window.size = (500, 500)
 
 
 class NavBar(FakeRectangularElevationBehavior, MDBoxLayout):
     pass
+
 
 class HomeScreen(Screen):
     pass
@@ -30,10 +35,14 @@ class MainApp(MDApp):
         self.theme_cls.primary_hue = "500"
         self.theme_cls.theme_style = "Light"
 
+        # start the gps
+        GpsHelper().run()
+
         # Intialise my database
         self.connection = RDSconnector("../../../conf.json")
+        self.cursor = self.connection.cursor
 
-        GpsHelper().run() # start the gps
+
 
 
 MainApp().run()
