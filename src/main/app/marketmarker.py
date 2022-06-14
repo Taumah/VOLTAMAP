5643c0dfab169aa67d9c9ef5b504b65ee42ce0bb
quentin-pierson <quentinprs62@gmail.com>
2022-06-14 11:20:18 +0200

    add popum menu to marker

--- src/main/app/marketmarker.py
+++ src/main/app/marketmarker.py
@@ -1,10 +1,13 @@
 from kivy_garden.mapview import MapMarkerPopup
 from kivy.properties import NumericProperty
 
+from locationpopupmenu import LocationPopupMenu
+
 
 class MarketMarker(MapMarkerPopup):
     lat = float
     lon = float
+    market_data = []
 
     def __init__(self, lat, lon, **kwargs):
         super().__init__(**kwargs)
@@ -14,4 +17,6 @@ class MarketMarker(MapMarkerPopup):
         self.texture_size = (25, 25)
 
     def on_release(self):
-        pass
+        menu = LocationPopupMenu(self.market_data)
+        menu.size_hint = [.8, .25]
+        menu.open()
